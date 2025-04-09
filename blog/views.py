2ca413e24d38c635eb110ad29b25d99b882804c3
Raphael Lins <rapharell06@gmail.com>
2025-04-08 22:48:38 -0300

    Implement product listing view and template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Produto
+
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'blog/listar_produtos.html', {'produtos': produtos})
 
 # Create your views here.
--- personal_blog/urls.py
+++ personal_blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('produtos/', views.listar_produtos, name='listar_produtos'),
 ]
